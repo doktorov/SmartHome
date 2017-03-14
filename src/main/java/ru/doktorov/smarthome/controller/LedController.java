f6ac5e8987ef81c9ec6c03460a1d5fab419d05ef
root <root@raspberrypi>
2017-03-15 00:23:52 +0600

    Switch

--- src/main/java/ru/doktorov/smarthome/controller/LedController.java
+++ src/main/java/ru/doktorov/smarthome/controller/LedController.java
@@ -1,13 +1,16 @@
 package ru.doktorov.smarthome.controller;
 
 import com.pi4j.io.gpio.*;
+import java.util.BitSet;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.doktorov.smarthome.rf.RCSwitch;
 
 @RestController
 public class LedController {
 
-    private static GpioPinDigitalOutput pin;
+    private static GpioPinDigitalOutput mPin01;
+    private static GpioPinDigitalOutput mPin07;
 
     @RequestMapping("/")
     public String greeting() {
@@ -16,13 +19,49 @@ public class LedController {
 
     @RequestMapping("/light")
     public String light() {
-        if (pin == null) {
+        if (mPin01 == null) {
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+            mPin01 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
         }
 
-        pin.toggle();
+        mPin01.toggle();
 
         return "OK";
+    }
+    
+    @RequestMapping("/switchOn")
+    public String switchOn() {
+        GpioController gpio = GpioFactory.getInstance();       
+
+        if (mPin07 == null) {
+            mPin07 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.LOW);
+        }
+        
+        BitSet address = RCSwitch.getSwitchGroupAddress("11111");
+        
+        RCSwitch transmitter = new RCSwitch(mPin07);
+        transmitter.switchOn(address, 3);
+        
+        gpio.shutdown();
+
+        return "switchOn";
+    }
+    
+    @RequestMapping("/switchOff")
+    public String switchOff() {       
+        GpioController gpio = GpioFactory.getInstance();       
+
+        if (mPin07 == null) {
+            mPin07 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.LOW);
+        }
+        
+        BitSet address = RCSwitch.getSwitchGroupAddress("11111");
+        
+        RCSwitch transmitter = new RCSwitch(mPin07);
+        transmitter.switchOff(address, 3);
+        
+        gpio.shutdown();
+
+        return "switchOff";
     }
 }
